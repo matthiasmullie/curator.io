8144680698f93537f687600a2aa7b01b90a6da14
dieterve <dieter@dieterve.be>
2012-06-24 04:54:57 +0200

    Collections: detail

--- default_www/modules/collections/actions/detail.php
+++ default_www/modules/collections/actions/detail.php
@@ -8,13 +8,49 @@
 class CollectionsDetail extends SiteBaseAction
 {
 	/**
+	 * @var Collection
+	 */
+	private $collection;
+
+	/**
+	 * @var User
+	 */
+	private $collectionOwner;
+
+	/**
 	 * Execute the action
 	 */
 	public function execute()
 	{
-		// @todo	better validation
-		$this->item = Collection::getByUri($this->url->getParameter(1));
+		/// exists
+		if(!CollectionsHelper::existsBySlug($this->url->getParameter(1)))
+		{
+			$this->redirect($this->url->buildUrl('index', 'error') . '?code=404&message=CollectionNotFoundError');
+		}
 
-		Spoon::dump($this->item);
+		// fetch data
+		$id = CollectionsHelper::getIdBySlug($this->url->getParameter(1));
+		$this->collection = Collection::get($id);
+		$this->collectionOwner = User::get($this->collection->user_id);
+
+		$this->parse();
+		$this->display();
+	}
+
+	/**
+	 * Parse template info.
+	 */
+	private function parse()
+	{
+		$this->tpl->assign('collection', $this->collection->toArray());
+		$this->tpl->assign('collectionOwner', $this->collectionOwner->toArray());
+
+		if(Authentication::getLoggedInUser())
+		{
+			if(Authentication::getLoggedInUser()->id == $this->collection->user_id)
+			{
+				$this->tpl->assign('isCollectionOwner', true);
+			}
+		}
 	}
 }
